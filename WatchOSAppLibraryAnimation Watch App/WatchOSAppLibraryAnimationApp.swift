285f6d843c1a3be7833cb8ccc123f75cb3a13832
ashhutoshpandey <ashutoshpandey.developer@gmail.com>
2026-03-22 11:11:57 +0530

    Initial Commit

---
+++ WatchOSAppLibraryAnimation Watch App/WatchOSAppLibraryAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchOSAppLibraryAnimationApp.swift
+//  WatchOSAppLibraryAnimation Watch App
+//
+//  Created by Ashutosh Pandey on 22/03/26.
+//
+
+import SwiftUI
+
+@main
+struct WatchOSAppLibraryAnimation_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
